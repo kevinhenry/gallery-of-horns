a557d15b2bd205f7ac87525d8a545d3c6a80ea28
Kevin Henry <kevinhenry@mac.com>
2021-04-01 01:47:40 -0700

    Dropdown Placeholder

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import CardColumns from 'react-bootstrap/CardColumns';
 // import data from './data.json';
+import FormComponent from './CounterHorn';
 import HornedBeasts from './HornedBeasts';
 
 import './Main.css';
 
 class Main extends React.Component {
   render () {
+    // let hornArray = this.props.horn.map((beast, index) => (
+    //   <HornedBeasts
+    //     name = {beast.keyword}
+    //     title = {beast.title}
+    //     image_url = {beast.image_url}
+    //     description = {beast.description}
+    //     key = {index}
+    //     handleClick={this.props.handleClick}
+    //   />
+      
+    // ));
+  
     let beastArray = this.props.data.map((beast, index) => (
       <HornedBeasts
         name = {beast.keyword}
@@ -15,14 +28,21 @@ class Main extends React.Component {
         description = {beast.description}
         key = {index}
         // showModal={this.props.showModal}
-        updateBeast={this.props.updateBeast}
+        // updateBeast={this.props.updateBeast}
         handleClick={this.props.handleClick}
       />
       
     ));
-  
+
     return (
       <main>
+      
+        <FormComponent>
+          {/* {hornArray} */}
+          filterBeast={this.props.filterBeast}
+          allBeast={this.props.allBeast}
+        </FormComponent>
+
         <CardColumns>
           {beastArray}
         </CardColumns>
